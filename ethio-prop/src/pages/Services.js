256255db6513ccc9e4073626d6e398d324c6803f
BekaHabesha <bereketdm1984@gmail.com>
2024-08-29 08:55:49 -0700

    For Getting Information about the services

---
+++ ethio-prop/src/pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div className="services">
+      <h1>Our Services</h1>
+      <p>We provide the best real estate services in Ethiopia, including property listing, buying, and selling services.</p>
+    </div>
+  );
+};
+
+export default Services;
