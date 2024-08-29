272b5f5a6dc098eee9b76cc1d176ea1ed735d55f
BekaHabesha <bereketdm1984@gmail.com>
2024-08-29 08:57:24 -0700

    For Getting Information about Ethio Properties

---
+++ ethio-prop/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <p>Ethio-Properties is a premier real estate platform in Ethiopia, offering comprehensive property listings, market insights, and customer service.</p>
+    </div>
+  );
+};
+
+export default About;
